b35c1d74081c6986c9537e0a0b69c990c1a93703
Taha Abid <47201449+tahaabid@users.noreply.github.com>
2022-01-26 16:17:16 +0800

    Update PingTransmmitterModulationTechnique2.ino
    added functionality to send bit signal using serial input

--- Arduino/Acoustic Modem/PingTransmmitterModulationTechnique2/PingTransmmitterModulationTechnique2.ino
+++ Arduino/Acoustic Modem/PingTransmmitterModulationTechnique2/PingTransmmitterModulationTechnique2.ino
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------- //
 // Arduino Ultrasoninc Sensor HC-SR04
-// Re-writed by 
+// Re-writed by
 // Using Arduino IDE 1.8.7
 // Using HC-SR04 Module
 // Tested on 17 September 2019
@@ -13,10 +13,10 @@
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
 
-#define t0 2
-#define t1 4
-#define tGuard 4
-
+#define t0 20
+#define t1 40
+#define tGuard 200
+char incomingByte;
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
@@ -26,85 +26,89 @@ void setup() {
   Serial.println("with Arduino UNO R3");
 }
 void loop() {
-//  // Clears the trigPin condition
-//  digitalWrite(trigPin, LOW);
-//  delay(20);
-//  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-//  digitalWrite(trigPin, HIGH);
-//  delay(20);
-//  digitalWrite(trigPin, LOW);
-//
-//  // Clears the trigPin condition
-//  digitalWrite(trigPin, LOW);
-//  delay(60);
-//  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-//  digitalWrite(trigPin, HIGH);
-//  delay(60);
-//  digitalWrite(trigPin, LOW);
-//
-//  // Clears the trigPin condition
-//  digitalWrite(trigPin, LOW);
-//  delay(180);
-//  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-//  digitalWrite(trigPin, HIGH);
-//  delay(180);
-//  digitalWrite(trigPin, LOW);
-  int t=20000;
+  //clear the buffer now
+  while (Serial.available()) {
+    Serial.print("Clearing the buffer: ");
+    Serial.println(Serial.read());
+  }
+  Serial.println("Waiting for serial input");
+  while (Serial.available() == 0) {} //Loop until serial command receieved
+  // read the incoming byte:
+  incomingByte = Serial.read();
+  Serial.print("I received: ");
+  Serial.println(incomingByte, BIN);
+
+
+  //  // Clears the trigPin condition
+  //  digitalWrite(trigPin, LOW);
+  //  delay(20);
+  //  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  //  digitalWrite(trigPin, HIGH);
+  //  delay(20);
+  //  digitalWrite(trigPin, LOW);
+  //
+  //  // Clears the trigPin condition
+  //  digitalWrite(trigPin, LOW);
+  //  delay(60);
+  //  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  //  digitalWrite(trigPin, HIGH);
+  //  delay(60);
+  //  digitalWrite(trigPin, LOW);
+  //
+  //  // Clears the trigPin condition
+  //  digitalWrite(trigPin, LOW);
+  //  delay(180);
+  //  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  //  digitalWrite(trigPin, HIGH);
+  //  delay(180);
+  //  digitalWrite(trigPin, LOW);
+  int t = 20000;
   //Clears the trigPin condition
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(tGuard);
-  digitalWrite(trigPin, HIGH);// 1 Start Bit
-  delay(t1);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
+  //  digitalWrite(trigPin, LOW);
+  //  delayMicroseconds(tGuard);
 
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, HIGH);// 1 Start Bit
-  delay(t1);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
+  for (int i = 0; i < 5000; i++) {
+    digitalWrite(trigPin, HIGH);// 1 Start Bit
+    delay(t1);
+    digitalWrite(trigPin, LOW);
+    delay(tGuard);
 
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, HIGH);// 0
-  delay(t0);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
+    //  digitalWrite(trigPin, LOW);
+    //  delayMicroseconds(t);
+    digitalWrite(trigPin, HIGH);// 1 Start Bit
+    delay(t1);
+    digitalWrite(trigPin, LOW);
+    delay(tGuard);
 
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, HIGH);// 0
-  delay(t0);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
+    for (int j = 6, l = 0; j >= 0; j--, l++) {
+      int sig = bitRead(incomingByte, j);  //the bit to send
+      if (i == 0) {  //only print the first time
+        Serial.print("Writing bit ");
+        Serial.print(l);
+        Serial.print(" -> ");
+        Serial.println(sig);
+      }
+      digitalWrite(trigPin, sig);
+      if (sig == 0) {
+        delay(t0);
+      }
+      else {
+        delay(t1);
+      }
+      digitalWrite(trigPin, !sig);
+      delay(tGuard);
+    }
 
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, HIGH);// 1
-  delay(t1);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
+    digitalWrite(trigPin, LOW);// 0 Stop bit
+    delay(tGuard);
+    digitalWrite(trigPin, LOW);
+    delay(tGuard);
 
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, HIGH);// 1
-  delay(t1);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
-
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, LOW);// 0 Stop bit
-  delay(tGuard);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
-  
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(t);
-  digitalWrite(trigPin, LOW);// 0 Stop bit
-  delay(tGuard);
-  digitalWrite(trigPin, LOW);
-  delay(tGuard);
-
+    //  digitalWrite(trigPin, LOW);
+    //  delayMicroseconds(t);
+    digitalWrite(trigPin, LOW);// 0 Stop bit
+    delay(tGuard);
+    digitalWrite(trigPin, LOW);
+    delay(tGuard);
+  }
 }
